748e558c8d0addde27db1a9e992b40708ba722c2
Joseph Guhlin <joseph.guhlin@gmail.com>
2023-10-29 09:56:52 +1300

    Update parse_fasta.rs

--- benches/parse_fasta.rs
+++ benches/parse_fasta.rs
@@ -6,13 +6,11 @@ fn criterion_benchmark(c: &mut Criterion) {
     let seq = include_str!("../bench_data/uniprot_sprot.fasta");
     let mut bufreader = std::io::BufReader::new(seq.as_bytes());
 
-    let mut group = c.benchmark_group("Parse UniProt SwissProt FASTA File");
-    group.throughput(criterion::Throughput::Bytes(seq.len() as u64));
-    group.bench_function("fffx_parse_fasta", |b| {
+    group.bench_function("needletail_parse_fasta", |b| {
         b.iter(|| {
-            let fasta = fffx::fasta::Fasta::from_buffer(black_box(&mut bufreader));
-            for seq in fasta {
-                let _ = seq.unwrap();
+            let mut fasta = needletail::parse_fastx_reader(black_box(&mut bufreader)).expect("Unable to parse");
+            while let Some(r) = fasta.next() {
+                let _ = r.unwrap();
             }
         })
     });
@@ -20,11 +18,13 @@ fn criterion_benchmark(c: &mut Criterion) {
     let seq = include_str!("../bench_data/uniprot_sprot.fasta");
     let mut bufreader = std::io::BufReader::new(seq.as_bytes());
 
-    group.bench_function("needletail_parse_fasta", |b| {
+    let mut group = c.benchmark_group("Parse UniProt SwissProt FASTA File");
+    group.throughput(criterion::Throughput::Bytes(seq.len() as u64));
+    group.bench_function("fffx_parse_fasta", |b| {
         b.iter(|| {
-            let mut fasta = needletail::parse_fastx_reader(black_box(&mut bufreader)).expect("Unable to parse");
-            while let Some(r) = fasta.next() {
-                let _ = r.unwrap();
+            let fasta = fffx::fasta::Fasta::from_buffer(black_box(&mut bufreader));
+            for seq in fasta {
+                let _ = seq.unwrap();
             }
         })
     });
