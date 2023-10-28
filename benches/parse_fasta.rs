4a0326aa29a3b1ed3f0283f4d41c0ca7641333f7
Joseph Guhlin <joseph.guhlin@gmail.com>
2023-10-29 10:21:53 +1300

    Update parse_fasta.rs

--- benches/parse_fasta.rs
+++ benches/parse_fasta.rs
@@ -3,14 +3,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let seq = include_str!("../bench_data/uniprot_sprot.fasta");
-    let mut bufreader = std::io::BufReader::new(seq.as_bytes());
-
     let mut group = c.benchmark_group("Parse UniProt SwissProt FASTA File");
-    group.throughput(criterion::Throughput::Bytes(seq.len() as u64));
+    let file_size = std::fs::metadata("bench_data/uniprot_sprot.fasta").unwrap().len();
+
+    group.throughput(criterion::Throughput::Bytes(file_size));
 
     group.bench_function("needletail_parse_fasta", |b| {
         b.iter(|| {
+            let fasta_file = std::fs::File::open("bench_data/uniprot_sprot.fasta").unwrap();
+            let mut bufreader = std::io::BufReader::new(fasta_file);
+
             let mut num_bases = 0;
             let mut fasta = needletail::parse_fastx_reader(black_box(seq.as_bytes())).expect("Unable to parse");
             while let Some(r) = fasta.next() {
@@ -20,11 +22,55 @@ fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 
-    let seq = include_str!("../bench_data/uniprot_sprot.fasta");
-    let mut bufreader = std::io::BufReader::new(seq.as_bytes());
-    
+    // let seq = include_str!("../bench_data/uniprot_sprot.fasta");
+    // let mut bufreader = std::io::BufReader::new(seq.as_bytes());
+   
     group.bench_function("fffx_parse_fasta", |b| {
         b.iter(|| {
+            let fasta_file = std::fs::File::open("bench_data/uniprot_sprot.fasta").unwrap();
+            let mut bufreader = std::io::BufReader::new(fasta_file);
+
+            let mut num_bases = 0;
+            let fasta = fffx::fasta::Fasta::from_buffer(black_box(&mut bufreader));
+            for seq in fasta {
+                let seq = seq.unwrap();
+                num_bases += seq.sequence.as_ref().unwrap().len();
+            }
+        })
+    });
+
+    group.finish();
+
+    // ----------------------------------------------
+    // Parse FASTQ
+    // ----------------------------------------------
+
+    let mut group = c.benchmark_group("Parse biofast-data-v1/M_abscessus_HiSeq.fq FASTQ");
+    let file_size = std::fs::metadata("bench_data/biofast-data-v1/M_abscessus_HiSeq.fq").unwrap().len();
+    group.throughput(file_size);
+
+    group.bench_function("needletail_parse_fastq", |b| {
+        b.iter(|| {
+            let fasta_file = std::fs::File::open("bench_data/biofast-data-v1/M_abscessus_HiSeq.fq").unwrap();
+            let mut bufreader = std::io::BufReader::new(fasta_file);
+
+            let mut num_bases = 0;
+            let mut fasta = needletail::parse_fastx_reader(black_box(seq.as_bytes())).expect("Unable to parse");
+            while let Some(r) = fasta.next() {
+                let seqrec = r.unwrap();
+                num_bases += seqrec.num_bases();
+            }
+        })
+    });
+
+    // let seq = include_str!("../bench_data/uniprot_sprot.fasta");
+    // let mut bufreader = std::io::BufReader::new(seq.as_bytes());
+   
+    group.bench_function("fffx_parse_fastq", |b| {
+        b.iter(|| {
+            let fasta_file = std::fs::File::open("bench_data/biofast-data-v1/M_abscessus_HiSeq.fq").unwrap();
+            let mut bufreader = std::io::BufReader::new(fasta_file);
+            
             let mut num_bases = 0;
             let fasta = fffx::fasta::Fasta::from_buffer(black_box(&mut bufreader));
             for seq in fasta {
